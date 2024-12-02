44180ff7eb82a98555279bde7cfde788dd542c72
pranshu baghel <devloperpranshu@gmail.com>
2024-12-02 21:52:40 +0530

    node modules

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,4 +1,18 @@
-const sum = require('./Sum')
-console.log("hello world")
-console.log(sum(4,6))
-console.log(sum(9,7))
+// const sum = require('./Sum')
+// console.log("hello world")
+// console.log(sum(4,6))
+// console.log(sum(9,7))
+
+// another way
+
+// const calculate = require('./Sum')
+// console.log('hello world')
+// console.log(calculate.sum(5,4))
+// console.log(calculate.subs(9,3))
+
+    // By using Javascript distruchring method
+    const calculate = require('./Sum')
+    const {sum, subs} = calculate;
+    console.log("hello world")
+    console.log(sum(6,4))
+    console.log(subs(7,3))
--- Sum.js
+++ Sum.js
@@ -1,4 +1,35 @@
-const sum = (a,b)=>{
+// const sum = (a,b)=>{
+//     return a+b;
+// };
+// module.exports = sum;
+
+// use another way...... with Sum And Substract
+
+// const sum = (a,b) => {
+//     return a+b;
+// }
+
+// const subs = (a,b) => {
+//     return a-b;
+// }
+
+// // with the help of object
+// module.exports = {
+//     sum : sum,
+//     subs: subs
+// };
+
+// if our Key and Value are same then we  can write as:
+// module.exports = {
+//     sum,
+//     subs
+// }
+
+// after destuctring method we can write
+
+module.exports.sum = (a,b) => {
     return a+b;
-};
-module.exports = sum;
+}
+module.exports.subs = (a,b) => {
+    return a-b;
+}
