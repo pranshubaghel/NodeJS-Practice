a823d0e4b10d6e5065959c75bd92cd69644e4b79
pranshu baghel <devloperpranshu@gmail.com>
2024-12-08 22:22:19 +0530

    path

--- FSindex.js
+++ FSindex.js
@@ -2,5 +2,18 @@ const fs = require('fs');
 
 //  synchronous way
 
-fs.writeFileSync('./sample.txt', "hey guys");
-console.log("File written successfully!");
+// fs.writeFileSync('./sample.txt', "hey guys");
+// console.log("File written successfully!");
+// fs.writeFileSync('./sample.txt', " pls subscribe", {flag:"a"})
+
+// Asynchronous way
+
+fs.writeFile('./sample.txt', "hey pranshu", (err) => {
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("file written")
+    }
+
+    })
---
+++ path.js
@@ -0,0 +1,5 @@
+const path = require("path")
+console.log(path.dirname('C:/Users/dell/OneDrive/Desktop/Nodejs/path.js'))
+console.log(path.extname('C:/Users/dell/OneDrive/Desktop/Nodejs/path.js'))
+console.log(path.basename('C:/Users/dell/OneDrive/Desktop/Nodejs/path.js'))
+console.log(path.parse('C:/Users/dell/OneDrive/Desktop/Nodejs/path.js'))
